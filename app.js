1f58a8ec43043b4e6510a6f63b8f97bc3cbf1d12
Lorenzo Spinelli <iam.lorenzo.spinelli@gmail.com>
2023-11-23 15:33:29 +0100

    feat: withdraw winings

--- app.js
+++ app.js
@@ -1,143 +1,165 @@
-const jsConfetti = new JSConfetti();
-const loginSection = document.getElementById("login");
-const controlsSection = document.getElementById("controls");
-const gameSection = document.getElementById("game");
-const lostSection = document.getElementById("lost");
-const loaderSection = document.getElementById("loader");
-const userSection = document.getElementById("user");
-const winnerSection = document.getElementById("winner");
+async function main() {
+  const jsConfetti = new JSConfetti();
+  const loginSection = document.getElementById("login");
+  const controlsSection = document.getElementById("controls");
+  const gameSection = document.getElementById("game");
+  const lostSection = document.getElementById("lost");
+  const loaderSection = document.getElementById("loader");
+  const userSection = document.getElementById("user");
+  const winnerSection = document.getElementById("winner");
 
-const loginButton = document.getElementById("login-button");
-const buyCardButton = document.getElementById("buy-card");
-const collectButton = document.getElementById("collect");
-const resetButton = document.getElementById("reset");
+  const loginButton = document.getElementById("login-button");
+  const buyCardButton = document.getElementById("buy-card");
+  const callBingoButton = document.getElementById("callbingo");
+  const resetButton = document.getElementById("reset");
+  const withdrawButton = document.getElementById("withdraw");
 
-const numbersElements = document.querySelectorAll(`[data-type="number"]`);
-const account = document.getElementById("account");
-const installMetamask = document.getElementById("install-metamask");
+  const numbersElements = document.querySelectorAll(`[data-type="number"]`);
+  const account = document.getElementById("account");
+  const installMetamask = document.getElementById("install-metamask");
 
-//TODO sono connesso alla rete giusta?
+  //TODO sono connesso alla rete giusta?
 
-const NUMBERS_REQUIRED = 6;
-let numbersIHave = 0;
+  const NUMBERS_REQUIRED = 6;
+  let numbersIHave = 0;
 
-function main() {
-  dapp
-    .isConnected()
-    .then((yes) => {
-      if (yes) {
+  function main() {
+    dapp
+      .isConnected()
+      .then((yes) => {
+        if (yes) {
+          hide(loginSection);
+          show(controlsSection);
+        } else {
+          show(loginButton);
+          show(loginSection);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        show(installMetamask);
+      })
+      .finally(() => {
+        hide(loader);
+      });
+  }
+
+  main();
+
+  function loginHandler() {
+    dapp.login().then((user) => {
+      if (user) {
         hide(loginSection);
         show(controlsSection);
-      } else {
-        show(loginButton)
-        show(loginSection);
+        show(userSection);
+        account.textContent = user;
       }
-    })
-    .catch((err) => {
-      console.log(err)
-      show(installMetamask);
-    })
-    .finally(() => {
-      hide(loader);
-    });
-}
-
-main();
-
-function loginHandler() {
-  dapp.login().then((user) => {
-    if (user) {
-      hide(loginSection);
-      show(controlsSection);
-      show(userSection)
-      account.textContent = user;
-    }
-  });
-}
-
-function buyBingoCardHandler() {
-  show(loaderSection)
-  dapp.buyBingoCard().then((card) => {
-    const { numbers } = card;
-    numbersElements.forEach((element, index) => {
-      element.textContent = numbers[index];
-    });
-    hide(controlsSection);
-    show(gameSection);
-  }).finally(() => {
-    hide(loaderSection)
-  });
-}
-
-function numberClickedHandler(event) {
-  const element = this;
-  element.classList.add("is-success");
-  launchConfetti();
-  numbersIHave++;
-  if (numbersIHave === NUMBERS_REQUIRED) {
-    displayWinButton();
-  }
-}
-
-function displayWinButton() {
-  show(collectButton, { mode: "inline-block" });
-}
-
-function collectButtonHandler() {
-  show(loaderSection)
-  dapp.collect().then((won) => {
-    if (won) {
-      launchConfetti(true);
-      show(winnerSection);
-      document.getElementById('dialog-default').showModal();
-    } else {
-      cheater();
-    }
-  }).finally(() => {
-    hide(loaderSection)
-  });
-}
-
-function reset() {
-  numbersIHave = 0;
-  hide(collectButton);
-  hide(lostSection);
-  numbersElements.forEach((element) => {
-    element.classList.remove("is-success");
-  });
-}
-
-function hide(element) {
-  element.classList.remove("block");
-  element.classList.remove("inline-block");
-  element.classList.add("hidden");
-}
-
-function show(element, options = { mode: "block" }) {
-  element.classList.remove("hidden");
-  element.classList.add(options.mode);
-}
-
-function launchConfetti(big = false) {
-  jsConfetti.addConfetti();
-  if (big) {
-    jsConfetti.addConfetti({
-      emojis: ["🌈", "⚡️", "💥", "✨", "💫", "🌸"],
-      confettiNumber: 100,
     });
   }
+
+  function buyBingoCardHandler() {
+    show(loaderSection);
+    dapp
+      .buyBingoCard()
+      .then((card) => {
+        const { numbers } = card;
+        numbersElements.forEach((element, index) => {
+          element.textContent = numbers[index];
+        });
+        hide(controlsSection);
+        show(gameSection);
+      })
+      .finally(() => {
+        hide(loaderSection);
+      });
+  }
+
+  function numberClickedHandler(event) {
+    const element = this;
+    element.classList.add("is-success");
+    launchConfetti();
+    numbersIHave++;
+    if (numbersIHave === NUMBERS_REQUIRED) {
+      displayWinButton();
+    }
+  }
+
+  function displayWinButton() {
+    show(callBingoButton, { mode: "inline-block" });
+  }
+
+  function callBingoButtonHandler() {
+    show(loaderSection);
+    dapp
+      .callBingo()
+      .then((won) => {
+        if (won) {
+          launchConfetti(true);
+          show(winnerSection);
+          document.getElementById("dialog-default").showModal();
+        } else {
+          cheater();
+        }
+      })
+      .finally(() => {
+        hide(loaderSection);
+      });
+  }
+
+  function withdrawListener() {
+    dapp
+      .withdraw()
+      .then(() => {
+        launchConfetti(true);
+      })
+      .catch((e) => {
+        console.log("cannot withdraw", e);
+      });
+  }
+
+  function reset() {
+    numbersIHave = 0;
+    hide(callBingoButton);
+    hide(lostSection);
+    numbersElements.forEach((element) => {
+      element.classList.remove("is-success");
+    });
+  }
+
+  function hide(element) {
+    element.classList.remove("block");
+    element.classList.remove("inline-block");
+    element.classList.add("hidden");
+  }
+
+  function show(element, options = { mode: "block" }) {
+    element.classList.remove("hidden");
+    element.classList.add(options.mode);
+  }
+
+  function launchConfetti(big = false) {
+    jsConfetti.addConfetti();
+    if (big) {
+      jsConfetti.addConfetti({
+        emojis: ["🌈", "⚡️", "💥", "✨", "💫", "🌸"],
+        confettiNumber: 100,
+      });
+    }
+  }
+
+  function cheater() {
+    show(lostSection);
+    document.getElementById("lost-dialog").showModal();
+  }
+
+  loginButton.addEventListener("click", loginHandler);
+  buyCardButton.addEventListener("click", buyBingoCardHandler);
+  numbersElements.forEach((element) =>
+    element.addEventListener("click", numberClickedHandler)
+  );
+  callBingoButton.addEventListener("click", callBingoButtonHandler);
+  resetButton.addEventListener("click", reset);
+  withdrawButton.addEventListener("click", withdrawListener);
 }
 
-function cheater() {
-  show(lostSection);
-  document.getElementById('lost-dialog').showModal();
-}
-
-
-loginButton.addEventListener("click", loginHandler);
-buyCardButton.addEventListener("click", buyBingoCardHandler);
-numbersElements.forEach((element) =>
-  element.addEventListener("click", numberClickedHandler)
-);
-collectButton.addEventListener("click", collectButtonHandler);
-resetButton.addEventListener("click", reset);
+addEventListener("DOMContentLoaded", main);
--- dapp.js
+++ dapp.js
@@ -1,33 +1,37 @@
 async function isConnected() {
-    return Promise.resolve(false);
-  }
-  
-  async function login() {
-    return Promise.resolve("0x8626f6940E2eb28930eFb4CeF49B2d1F2C9C1199")
-  }
-  
-  async function buyBingoCard() {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        resolve({
-          numbers: [3, 23, 12, 34, 9, 1],
-        });
-      }, 1000);
-    });
-  }
-  
-  async function collect() {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        resolve(resolve(Math.random() > .5));
-      }, 1000);
-    });
-  }
-  
-  window.dapp = {
-    buyBingoCard,
-    isConnected,
-    collect,
-    login,
-  };
-  
+  return Promise.resolve(false);
+}
+
+async function login() {
+  return Promise.resolve("0x8626f6940E2eb28930eFb4CeF49B2d1F2C9C1199");
+}
+
+async function buyBingoCard() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve({
+        numbers: [3, 23, 12, 34, 9, 1],
+      });
+    }, 1000);
+  });
+}
+
+async function callBingo() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(resolve(Math.random() > 0.5));
+    }, 1000);
+  });
+}
+
+async function withdraw() {
+  return Promise.resolve();
+}
+
+window.dapp = {
+  buyBingoCard,
+  isConnected,
+  callBingo,
+  login,
+  withdraw
+};
